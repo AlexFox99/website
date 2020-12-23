3ef2632b5cb4a7da4211645e81e50a022e8a26c6
AlexFox(Aleksei_Dubynin) <66352898+AlexFox99@users.noreply.github.com>
2020-12-23 17:02:14 +0700

    delite none use

--- src/index.js
+++ src/index.js
@@ -4,25 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-let LoginPlaceholder="Введите имя";
-let PasswordPlaceholder="Введите пароль";
-let ImageSrc="https://yt3.ggpht.com/a/AATXAJxNghAFcCvYyall2kD_Gc3S62I7eqCvKu_QNbn9=s900-c-k-c0xffffffff-no-rj-mo";
-let TypeInput="text";
-let ClassNameInput="input";
-let ClassNameImg="logo_my";
-
-let LinkNameRegistration="Регистрация";
-let LinkToRegistration="/Authorization";
-let ClassNamePassword="reg_pass";
-let PasswordRepeatPlaceholderRegistration="Повторите пароль";
-
-let LinkToForgotEntrance="/ForgotPassword";
-let LinkNameForgotEntrance="Забыли пароль?";
-let LinkNameEnterEntrance="Войти";
-let LinkToEnterEntrance="/Map";
-let ClassNamePasswordEntrance="ent_pass";
-
 ReactDOM.render(
   <React.StrictMode>
           <App/>
