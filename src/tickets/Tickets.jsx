6c415f4f63984d65d63b1c3e7cd814c3a8ea860f
AlexFox(Aleksei_Dubynin) <66352898+AlexFox99@users.noreply.github.com>
2021-01-07 19:39:20 +0700

    Refactor page tickets

--- src/tickets/Tickets.jsx
+++ src/tickets/Tickets.jsx
@@ -3,16 +3,21 @@ import Ticket from "./ticket/Ticket";
 import "./tickets.css"
 const Tickets = () => {
     return(
-        <div className="map-wrapper">
-            <div className="map-header">
-                I map
-            </div>
-            <div className="map-content">
-                <Ticket/>
-            </div>
-
-            <div className="map-nav">
-                It is info!
+        <div className="TicketsPage">
+            <div className="TicketsContent">
+                <div className="NameTickets">Заявки</div>
+                <div className="Name1Tickets">Район:<select>
+                    <option value="1">Железнодорожный</option>
+                    <option value="2">Кировский</option>
+                    <option value="3">Ленинский</option>
+                    <option value="4">Октябрьский</option>
+                    <option value="5">Свердловский</option>
+                    <option value="6">Советский</option>
+                    <option value="7">Центральный</option>
+                </select></div>
+                <div className="TicketScroll">
+                    <Ticket/>
+                </div>
             </div>
         </div>
     );
--- src/tickets/ticket/Ticket.jsx
+++ src/tickets/ticket/Ticket.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import "./ticket.css"
 const Ticket = () => {
     return(
-        <div className="direct-wrapper">
-            <div onClick={()=>alert("d1")} className="direct-1">I Direct 1</div>
-            <div className="direct-2">I Direct 2</div>
-            <div className="direct-3">I Direct 3</div>
-            <div className="direct-4">I Direct 4</div>
-            <div className="direct-5">I Direct 5</div>
-            <div className="direct-6">I Direct 6</div>
-            <div className="direct-7">I Direct 7</div>
+        <div className="row">
+            <div className="Tickets1Content">1 заявка
+                <input  className="имя" type="radio" value="1"/>поступила
+                <input className="имя" type="radio" value="2"/>в обработке
+                <input className="имя" type="radio" value="3"/>выполнена
+            </div>
+            <div className="Tickets1Content">2 заявка
+                <input checked="checked" className="имя" type="radio" value="1"/>поступила
+                <input className="имя" type="radio" value="2"/>в обработке
+                <input className="имя" type="radio" value="3"/>выполнена
+            </div>
+            <div className="Tickets1Content">3 заявка
+                <input checked="checked" className="имя" type="radio" value="1"/>поступила
+                <input className="имя" type="radio" value="2"/>в обработке
+                <input className="имя" type="radio" value="3"/>выполнена
+            </div>
+            <div className="Tickets1Content">3 заявка
+                <input checked="checked" className="имя" type="radio" value="1"/>поступила
+                <input className="имя" type="radio" value="2"/>в обработке
+                <input className="имя" type="radio" value="3"/>выполнена
+            </div>
+            <div className="Tickets1Content">3 заявка
+                <input checked="checked" className="имя" type="radio" value="1"/>поступила
+                <input className="имя" type="radio" value="2"/>в обработке
+                <input className="имя" type="radio" value="3"/>выполнена
+            </div>
         </div>
     );
 }
