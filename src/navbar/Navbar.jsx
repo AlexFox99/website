d1253221558fb22685d31c321650a1ba0555691f
AlexFox(Aleksei_Dubynin) <66352898+AlexFox99@users.noreply.github.com>
2021-01-25 18:05:39 +0700

    initial

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,22 +3,25 @@ import s from "./Navbar.module.css";
 import {NavLink} from "react-router-dom";
 
 
-let Navbar =(props)=>{
-    let visible=()=>{
+let Navbar = (props) => {
+    let visible = () => {
         debugger
-        if(props.buttonVisible===true){
-            return(<div className={s.App_Nav}><NavLink className={s.NavLink} to={props.links}>{props.links}</NavLink>
+        if (props.buttonVisible === true) {
+            return (<div className={s.App_Nav}><NavLink className={s.NavLink} to={props.links}>{props.links}</NavLink>
                 <div className={s.User_Text}><p className={s.User}>{props.user}</p></div>
-                <div className={s.ForBut}><button className={s.button}>{"Logout"}</button></div>
-                </div>);
-        }
-        else {
-            return(<NavLink className={s.NavLink} to={props.links}>{props.links}</NavLink>)
+                <div className={s.ForBut}>
+                    <button className={s.button}>{"Logout"}</button>
+                </div>
+            </div>);
+        } else {
+            return (<div className={s.App_Nav}>
+                <NavLink className={s.NavLink} to={props.links}>{props.links}</NavLink>
+            </div>)
         }
     }
-        return (<div>
-                {visible()}
-            </div>
-        );
+    return (<div>
+            {visible()}
+        </div>
+    );
 }
 export default Navbar;
