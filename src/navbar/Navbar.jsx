f2616161bbc6b859c0532e9b281acf8685976926
AlexFox(Aleksei_Dubynin) <66352898+AlexFox99@users.noreply.github.com>
2021-01-26 11:41:29 +0700

    loqout

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import s from "./Navbar.module.css";
-import {NavLink} from "react-router-dom";
-
+import {NavLink, withRouter} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import * as axios from "axios";
 
 let Navbar = (props) => {
+    const history = useHistory();
+    let ClickLogOut=()=>{
+        axios.post("http://84.22.135.132:5000/WebUser/Logout", [{'Content-Type': 'application/json'}])
+            .then(res => {
+                debugger
+                if (res.data.message === null) {
+                    debugger
+                    alert(res.data.error);
+                } else if (res.data.error === null) {
+                    debugger
+                    alert(res.data.message);
+                    history.push("/Authorization/Entrance");
+                }
+            });
+    }
     let visible = () => {
         debugger
         if (props.buttonVisible === true) {
             return (<div className={s.App_Nav}><NavLink className={s.NavLink} to={props.links}>{props.links}</NavLink>
                 <div className={s.User_Text}><p className={s.User}>{props.user}</p></div>
                 <div className={s.ForBut}>
-                    <button className={s.button}>{"Logout"}</button>
+                    <button className={s.button} onClick={ClickLogOut}>{"Logout"}</button>
                 </div>
             </div>);
         } else {
@@ -24,4 +40,4 @@ let Navbar = (props) => {
         </div>
     );
 }
-export default Navbar;
+export default withRouter(Navbar);
